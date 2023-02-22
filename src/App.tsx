a8e061162e99500b9f0ed7a9891f6b02dd2f44bd
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2023-02-21 19:03:25 -0500

    config tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-
 function App(): JSX.Element {
-  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((countRef) => countRef + 1)}
-          type="button"
-        >
-          count is {count}
-        </button>
-        <p>Edit src/App.tsx and save to test HMR</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Hola</h1>
+      <h1 className=" text-sky-400 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p className="bg-sky-400">The quick brown fox...</p>
     </div>
   );
 }
