a138088c1e155b99dfca63c67a56b7bf0088fedf
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2023-02-23 21:32:22 -0500

    add resolutions pack

--- src/pages/dashboard/components/MenuOptions.tsx
+++ src/pages/dashboard/components/MenuOptions.tsx
@@ -1,11 +1,15 @@
 import { AccordionMenu, AccordionItem } from '../../../components/Accordion';
 import TailButton from '../../../components/Buttons';
+import sizes from '../../../assets/resolutions.json';
 
 function MenuOptions(): JSX.Element {
+  console.log(sizes);
   return (
     <AccordionMenu>
-      <AccordionItem title="Width">
-        <TailButton title="Test" />
+      <AccordionItem title="Sizes">
+        {sizes.sizes.map((size) => (
+          <TailButton title={size.name} />
+        ))}
       </AccordionItem>
     </AccordionMenu>
   );
